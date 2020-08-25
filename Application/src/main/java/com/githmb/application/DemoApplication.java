5b7872478794551fd1e825d857273a819520bca7
develon2015 <develon@qq.com>
2020-08-25 09:32:18 +0800

    Use Shell

--- Application/src/main/java/com/githmb/application/DemoApplication.java
+++ Application/src/main/java/com/githmb/application/DemoApplication.java
@@ -3,9 +3,12 @@ package com.githmb.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static develon.spring.log.LoggerKt.getLog;
+
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
+        getLog().d("你好");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
--- Application/src/main/java/com/githmb/application/DemoController.java
+++ Application/src/main/java/com/githmb/application/DemoController.java
@@ -1,7 +1,9 @@
 package com.githmb.application;
 
 import develon.spring.Init;
+import develon.spring.process.Shell;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 class DemoMessage {
@@ -21,5 +23,10 @@ public class DemoController {
     @RequestMapping("/**")
     public DemoMessage test() {
         return new DemoMessage(mySpringBoot.getVersion(), mySpringBoot.getAuthorName());
+    }
+
+    @RequestMapping("/sh")
+    public String sh(@RequestParam String cmd) {
+        return new Shell().ready().run(cmd, 2000, 100);
     }
 }
